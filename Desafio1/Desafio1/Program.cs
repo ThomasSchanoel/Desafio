c35866597affd8e26ce556427375409343d5564a
Thomas Schanoel <schanoel500@gmail.com>
2024-10-21 19:37:18 -0300

    Adicionando os Arquivos

---
+++ Desafio1/Desafio1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Desafio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite um número: ");
+            int num = int.Parse(Console.ReadLine());
+
+            if (PertenceFib(num))
+            {
+                Console.WriteLine($"O número {num} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {num} não pertence à sequência de Fibonacci.");
+            }
+
+        }
+
+        static bool PertenceFib(int num)
+        {
+            int a = 0, b = 1, c = 0;
+
+            while (c < num)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+
+            return c == num || num == 0;
+        }
+    }
+}
---
+++ Desafio2/Desafio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Desafio2
+{
+    static void Main()
+    {
+        Console.Write("Digite o texto: ");
+        string texto = Console.ReadLine();
+
+
+        int cont = 0;
+
+        foreach (char c in texto)
+        {
+            if (c == 'a' || c == 'A')
+            {
+                cont++;
+            }
+        }
+
+        if (cont > 0)
+        {
+            Console.WriteLine($"A letra 'a' aparece {cont} vez(es) no texto inserido.");
+        }
+        else
+        {
+            Console.WriteLine("A letra 'a' não aparece no texto inserido.");
+        }
+    }
+
+}
